55f083cb13a49e9694c4b9f43f7ecdc169fe971e
Haihao Yan <hyan36@gmal.com>
2017-12-04 23:19:11 +1100

    reformat

--- src/boardgame/BoardApp.java
+++ src/boardgame/BoardApp.java
@@ -18,40 +18,40 @@ import javafx.stage.Stage;
 
 public class BoardApp extends Application {
 
-    private Stage primaryStage;
-    private Pane rootLayout;
-    private Pane boardLayout;
-    private Map<Enum<?>, String> config;
-    private AbstractController rootController; 
-    private AbstractController boardController;
+	private Stage primaryStage;
+	private Pane rootLayout;
+	private Pane boardLayout;
+	private Map<Enum<?>, String> config;
+	private AbstractController rootController;
+	private AbstractController boardController;
 
-    public BoardApp(){
-    	config = new HashMap<Enum<?>, String>();
-    	
-    	// should load width and height from a configuration file
-    	config.put(Config.WINDOW_HEIGHT, "900");
-    	config.put(Config.WINDOW_WIDTH, "900");
-    	config.put(Config.TITLE, "Board Game App");
-    	
-    	rootController = new RootController(config);
-    	boardController = new BoardController(config, new PieceBuilder());
-    	
-    	boardController.addController(Config.ROOTCONTROLLER, rootController);
-    	rootController.addController(Config.BORDCONTROLLER, boardController);
-    	
-    	rootLayout = this.loadPane("views/RootLayout.fxml", this.rootController);
-    	boardLayout = this.loadPane("views/BoardView.fxml", this.boardController);
-    	
-    }
-    
-	@Override
-	public void start(Stage primaryStage) {		
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle(this.config.get(Config.TITLE));
-        initRootLayout();
-        showBoardView();
+	public BoardApp() {
+		config = new HashMap<Enum<?>, String>();
+
+		// should load width and height from a configuration file
+		config.put(Config.WINDOW_HEIGHT, "900");
+		config.put(Config.WINDOW_WIDTH, "900");
+		config.put(Config.TITLE, "Board Game App");
+
+		rootController = new RootController(config);
+		boardController = new BoardController(config, new PieceBuilder());
+
+		boardController.addController(Config.ROOTCONTROLLER, rootController);
+		rootController.addController(Config.BORDCONTROLLER, boardController);
+
+		rootLayout = this.loadPane("views/RootLayout.fxml", this.rootController);
+		boardLayout = this.loadPane("views/BoardView.fxml", this.boardController);
+
 	}
-	
+
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle(this.config.get(Config.TITLE));
+		initRootLayout();
+		showBoardView();
+	}
+
 	/**
 	 * 
 	 * @param fxml
@@ -61,9 +61,9 @@ public class BoardApp extends Application {
 	 */
 	protected Pane loadPane(String fxml, AbstractController controller) {
 		// Load root layout from FXML file.
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(BoardApp.class.getResource(fxml));
-        loader.setController(controller);
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(BoardApp.class.getResource(fxml));
+		loader.setController(controller);
 		try {
 			return (Pane) loader.load();
 		} catch (IOException e) {
@@ -72,16 +72,16 @@ public class BoardApp extends Application {
 		}
 	}
 
-    public void initRootLayout() {
+	public void initRootLayout() {
 		// Show the scene containing the root layout.
 		Scene scene = new Scene(rootLayout);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-    }
+	}
 
-    public void showBoardView() {
+	public void showBoardView() {
 		((BorderPane) rootLayout).setLeft(this.boardLayout);
-    }
+	}
 
 	public static void main(String[] args) {
 		launch(args);
